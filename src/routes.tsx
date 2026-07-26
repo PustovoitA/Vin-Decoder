56b734de5a8341cadd3d2491175da58f1d2eaa37
Andrei Pustovoit <andreipustovoit6@gmail.com>
2026-07-26 12:41:08 +0300

    setup routing

---
+++ src/routes.tsx
@@ -0,0 +1,22 @@
+import { Navigate, Route, Routes } from "react-router-dom"
+import Home from "./pages/Home/Home"
+
+
+const AppRoutes = () => {
+    const navigationRoutes = [
+        {
+            path: "/",
+            element: <Navigate to = "Home"/>
+        },
+        {
+            path: "/",
+            element: <Home/>
+        }
+    ]
+
+    return <Routes>{navigationRoutes.map(route => 
+        <Route key={route.path} path={route.path} element={route.element}/>
+    )}</Routes>
+}
+
+export default AppRoutes
